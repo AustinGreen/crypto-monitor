e260c979812690ba2e0360aaa933ea165d3dc0ef
Austin Green <austinzgreen@gmail.com>
2017-06-23 19:19:37 -0400

    price button for prices

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import currencyFormatter from 'currency-formatter';
 
-const Prices = ({ currencyAmounts }) =>
-  <section className="section" style={{ backgroundColor: '#f5f5f5', minHeight: '100vh' }}>
-    <div className="container">
-      <h1 className="title">Prices</h1>
-      <ul>
-        {currencyAmounts.map(amount => <li key={amount.name}>{amount.name}: $2,000</li>)}
-      </ul>
-    </div>
-  </section>;
+class Prices extends Component {
+  componentDidMount() {
+    this.fetchPrices();
+  }
+
+  fetchPrices(currencyName) {
+    fetch(`https://min-api.cryptocompare.com/data/price?fsym=${currencyName}&tsyms=USD`)
+      .then(response => response.json())
+      .then(result => console.log(currencyFormatter.format(result.USD, { code: 'USD' })));
+  }
+
+  render() {
+    const { currencyAmounts } = this.props;
+    return (
+      <section className="section" style={{ backgroundColor: '#f5f5f5', minHeight: '100vh' }}>
+        <div className="container">
+          <h1 className="title">Prices</h1>
+          <ul>
+            {currencyAmounts.map(amount =>
+              <li key={amount.name}>{amount.name}: <a onClick={() => this.fetchPrices(amount.name)}>Get Price</a></li>
+            )}
+          </ul>
+        </div>
+      </section>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   currencyAmounts: state.amounts
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,14 +1,10 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
-// import { Router, Route } from 'react-router';
 import App from './App';
 
 const Root = ({ store }) =>
   <Provider store={store}>
-    {/* <Router> */}
     <App store={store} />
-    {/* <Route exact path="/" component={App} /> */}
-    {/* </Router> */}
   </Provider>;
 
 Root.propTypes = {

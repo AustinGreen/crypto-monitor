19614bdb0dbe1662f853ff72db7eba060b8cb5eb
Austin Green <austinzgreen@gmail.com>
2017-08-10 18:16:31 -0400

    log actions

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,16 +3,37 @@ import throttle from 'lodash/throttle';
 import portfolioApp from './reducers';
 import { loadState, saveState } from './localStorage';
 
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch;
+  if (!console.group) {
+    return rawDispatch;
+  }
+
+  return (action) => {
+    console.group(action.type);
+    console.log('%c prev state', 'color: gray', store.getState());
+    console.log('%c action', 'color: orange', action);
+    const returnValue = rawDispatch(action);
+    console.log('%c next state', 'color: white', store.getState());
+    console.groupEnd(action.type);
+    return returnValue;
+  };
+};
+
 const configureStore = () => {
   const persistedState = loadState();
   const store = createStore(portfolioApp, persistedState);
 
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store);
+  }
+
   store.subscribe(
     throttle(() => {
       saveState({
-        amounts: store.getState().amounts
+        amounts: store.getState().amounts,
       });
-    }, 1000)
+    }, 1000),
   );
 
   return store;

972b437e936587d40c96fec41cc1155f8df54c7d
Austin Green <austinzgreen@gmail.com>
2017-08-19 23:20:06 -0400

    experimenting with scokets

---
+++ server.js
@@ -0,0 +1,14 @@
+const io = require('socket.io')();
+
+io.on('connection', (client) => {
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+});
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port ', port);

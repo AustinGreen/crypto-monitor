0364f370a606baf5abb8f802c1c5bb911f5b21db
Austin Green <austinzgreen@gmail.com>
2018-02-03 10:35:04 -0500

    fix react router config

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,29 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import Hero from './Hero';
 import Dashboard from './Dashboard';
+import Portfolio from './Portfolio';
+import Prices from './Prices';
 
 const App = () => (
   <div>
     <Hero />
-    <Dashboard>
-      <Route exact path="/" render={() => <Redirect to="/prices" />} />
-      <Route path="/prices" />
-      <Route path="/portfolio" />
-    </Dashboard>
+    <Route exact path="/" render={() => <Redirect to="/prices" />} />
+    <Route
+      path="/prices"
+      render={() => (
+        <Dashboard>{(_, currencyData, prices) => <Prices currencyData={currencyData} prices={prices} />}</Dashboard>
+      )}
+    />
+    <Route
+      path="/portfolio"
+      render={() => (
+        <Dashboard>
+          {(fetchPrices, currencyData, prices) => (
+            <Portfolio fetchPrices={fetchPrices} currencyData={currencyData} prices={prices} />
+          )}
+        </Dashboard>
+      )}
+    />
   </div>
 );
 
